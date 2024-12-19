3d403bc9a98181e50706b06012ed095e0c689fc5
lucumt <lucumt@gmail.com>
2024-12-19 19:39:23 +0800

    允许cors跨域查询

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var pcdsRouter = require('./routes/pcds');
+var cors = require('cors')
 
 var app = express();
 
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({credentials: true, origin: true}));
 
 app.use('/', indexRouter);
 app.use('/pcds', pcdsRouter);
