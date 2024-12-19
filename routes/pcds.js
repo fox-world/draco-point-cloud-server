df973015ae17540c868e6ed3a174d16ab8763edd
lucumt <lucumt@gmail.com>
2024-12-19 18:56:46 +0800

    添加点云文件

--- routes/pcds.js
+++ routes/pcds.js
@@ -7,3 +7,25 @@ router.get('/loadPcdBinary', function(req, res, next) {
 });
 
 module.exports = router;
+
+
+var fs = require('fs');
+var readline = require('readline');
+
+async function processPcdFile() {
+  const fileStream = fs.createReadStream('input.txt');
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+  // Note: we use the crlfDelay option to recognize all instances of CR LF
+  // ('\r\n') in input.txt as a single line break.
+
+  for await (const line of rl) {
+    // Each line in input.txt will be successively available here as `line`.
+    console.log(`Line from file: ${line}`);
+  }
+}
+
+processLineByLine();
